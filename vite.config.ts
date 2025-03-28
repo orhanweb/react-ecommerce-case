00d9a68f6960a885220ceba573feb2383c075109
Orhan <herorhan999@gmail.com>
2025-03-28 21:35:53 +0300

    Settings v2 for deploying with gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hyper-store/',
+  base: '/react-ecommerce-case/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
